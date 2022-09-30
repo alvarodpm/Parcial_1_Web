3040f1e98288e960b74ed4838d353c760ebc6e51
Alvaro Plata <ad.plata@uniandes.edu.co>
2022-09-30 07:46:48 -0500

    Logica completa

--- src/cafe-tienda/cafe-tienda.service.spec.ts
+++ src/cafe-tienda/cafe-tienda.service.spec.ts
@@ -1,8 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CafeEntity } from 'src/cafe/cafe.entity';
+import { Repository } from 'typeorm';
 import { CafeTiendaService } from './cafe-tienda.service';
 
 describe('CafeTiendaService', () => {
   let service: CafeTiendaService;
+  let repository: Repository<CafeEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
